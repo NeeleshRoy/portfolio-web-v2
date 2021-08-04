e48d4054b6e3f61a6d8f840c2f2d41d6fd162d88
neeleshroy <neeleshroy.2013@gmail.com>
2021-08-04 23:38:02 +0530

    husky commit check

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
-import path from "path";
-import babel from "@rollup/plugin-babel";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import copy from 'rollup-plugin-copy'
-import replace from "@rollup/plugin-replace";
-import { terser } from "rollup-plugin-terser";
-import json from "@rollup/plugin-json";
-import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
-import postcss from "rollup-plugin-postcss";
+import path from 'path';
+import babel from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
+import replace from '@rollup/plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import postcss from 'rollup-plugin-postcss';
 import simplevars from 'postcss-simple-vars';
 import nested from 'postcss-nested';
 import cssnext from 'postcss-cssnext';
@@ -16,62 +16,63 @@ import cssnano from 'cssnano';
 
 const dotenv = require('dotenv');
 
-const isProduction = process.env.BUILD === "production";
-const isDev = process.env.BUILD === "development";
+const isProduction = process.env.BUILD === 'production';
+const isDev = process.env.BUILD === 'development';
 const plugins = [
-    postcss({
-        extensions: [".sass"],
-        plugins:[
-            simplevars(),
-            nested(),
-            cssnext({ warnForDuplicates: false }),
-            cssnano()
-        ]
-    }),
-    replace({
-        'process.env.NODE_ENV': JSON.stringify(dotenv.config().parsed.NODE_ENV),
-        preventAssignment: true
-    }),
-    nodeResolve({
-        extensions: ['.js', '.jsx']
-    }),
-    json(),
-    babel({
-        exclude: "node_modules/**",
-        babelHelpers: "bundled",
-        envName: "client",
-        configFile: path.resolve(__dirname, "babel.config.js")
-    }),
-    commonjs(),
-    isProduction && terser(),
-    copy({
-        targets: [
-            { src: 'public/index.html', dest: 'dist' },
-        //   { src: ['assets/fonts/arial.woff', 'assets/fonts/arial.woff2'], dest: 'dist/public/fonts' },
-        //   { src: 'assets/images/**/*', dest: 'dist/public/images' }
-        ]
-    })
+  postcss({
+    extensions: ['.scss'],
+    plugins: [
+      simplevars(),
+      nested(),
+      cssnext({ warnForDuplicates: false }),
+      cssnano(),
+    ],
+  }),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(dotenv.config().parsed.NODE_ENV),
+    preventAssignment: true,
+  }),
+  nodeResolve({
+    extensions: ['.js', '.jsx'],
+  }),
+  json(),
+  babel({
+    exclude: 'node_modules/**',
+    babelHelpers: 'bundled',
+    envName: 'client',
+    configFile: path.resolve(__dirname, 'babel.config.js'),
+  }),
+  commonjs(),
+  isProduction && terser(),
+  copy({
+    targets: [
+      {
+        src: 'public/**/*',
+        dest: 'dist',
+      },
+    ],
+  }),
 ];
 
 if (isDev) {
-    const serverOptions = {
-        open: true,
-        verbose: true,
-        contentBase: ["dist"],
-        historyApiFallback: true,
-        host: 'localhost',
-        port: 3000
-    };
-    plugins.push(serve(serverOptions));
-    plugins.push(livereload({ watch: 'dist' }));
+  const serverOptions = {
+    open: true,
+    verbose: true,
+    contentBase: ['dist'],
+    historyApiFallback: true,
+    host: 'localhost',
+    port: 3000,
+  };
+  plugins.push(serve(serverOptions));
+  plugins.push(livereload({ watch: 'dist' }));
 }
 
 export default {
-    input: path.resolve(__dirname, "src/index.js"),
-    plugins,
-    output: {
-        file: path.resolve(__dirname, "dist/bundle.js"),
-        format: "iife",
-        name: "bundle"
-    }
-}
+  input: path.resolve(__dirname, 'src/index.js'),
+  plugins,
+  output: {
+    file: path.resolve(__dirname, 'dist/bundle.js'),
+    format: 'iife',
+    name: 'bundle',
+  },
+};
---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../sass/index.scss';
+
+const App = () => (
+  <div>
+    Neelesh Roy - Portfolio V2
+  </div>
+);
+
+export default App;
